aedcc1ce5ac8c79093518604b5a230e580ed2878
Hedeiwy <mhedeiwym@gmail.com>
2020-11-24 16:35:10 +0800

    Numpy

---
+++ Numpy.py
@@ -0,0 +1,8 @@
+from numpy import *
+# Numpy is to work with multidemensinal arrays
+
+arr = array([1,2,3,4])
+print(arr)
+arr.
+
+# Check this to checkout the different ways of creating arraysin numpy https://www.youtube.com/watch?v=NYPKbmE0H6E&list=PLsyeobzWxl7poL9JTVyndKe62ieoN-MZ3&index=33
