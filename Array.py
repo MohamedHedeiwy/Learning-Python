e2403c6fb5f03fe215a1f73209ac6bbe289713c8
Hedeiwy <mhedeiwym@gmail.com>
2020-11-24 14:36:59 +0800

    Two ways of looping using for loop
    used for else

--- Array.py
+++ Array.py
@@ -1,4 +1,26 @@
 from array import *
 
-vals = array()
 
+# the i is to specify the type of the array wich is int
+vals = array('i', [1, 2, 3, 4])
+
+for i in range(len(vals)):
+    print(vals[i])
+
+print()
+
+# Copying an Array
+newArr = array(vals.typecode, (a*a for a in vals))   
+# this is more efficent but you dont use index to grab values
+for i in newArr:
+    if i == 88:
+        print(i)
+        break
+# For else
+else:
+    print('Not Found')
+       
+
+ 
+
+# Check this if you forgot https://www.youtube.com/watch?v=6a39OjkCN5I&list=PLsyeobzWxl7poL9JTVyndKe62ieoN-MZ3&index=30
