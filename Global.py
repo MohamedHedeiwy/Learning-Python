840f7e5fe1011388773ce07907f13a08609d0397
Hedeiwy <mhedeiwym@gmail.com>
2020-11-25 23:16:28 +0800

    Global & local Variables

---
+++ Global.py
@@ -0,0 +1,27 @@
+a = 100
+
+def no_gloabal():
+    a =120
+    print('inside no gloabal',a)
+
+def test():
+    # this allows you to access the global variable
+    global a
+    a = 13
+    print('inside test',a)
+
+
+test()
+print('outside function',a)
+no_gloabal()
+
+
+
+def somethiong():
+    a = 12
+    # if you have the same name for a local and gloabal variable you can use gloabals to only change the gloable value wothout affecting the local
+    globals()['a'] = 14
+    print(a)
+    print(globals()['a'])
+
+print(somethiong())
