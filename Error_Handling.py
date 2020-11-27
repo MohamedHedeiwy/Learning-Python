717e97d453010e97e21e2f5bd2cc6ba32e5ebb66
Hedeiwy <mhedeiwym@gmail.com>
2020-11-28 00:05:55 +0800

    pylint
    looks for errors

--- Error_Handling.py
+++ Error_Handling.py
@@ -1,4 +1,4 @@
-def add(a,b):
+def add(a, b):
     try:
         print(a+b)
     except:
@@ -6,7 +6,8 @@ def add(a,b):
     else:
         print('you got no error')
 
-def add1(a,b):
+
+def add1(a, b):
     try:
         x = a+b
     except:
@@ -15,23 +16,37 @@ def add1(a,b):
         print('you got no error')
         return print(x)
 
+
 print('add')
-add(12,2)
+add(12, 2)
 
 print()
 
 print('add1')
-add1(12,2)
+add1(12, 2)
+
 
 def in1():
     while True:
+        # you want to be specific on what you are trying to catch dont simply put all your code here
+        # you may catch an excepetion but not one that was expected 
+        # so its better to only use it if you are expecting an error
+        # then you can continue with yuor statments in the else section after the exception
         try:
-            x = int(input('enter number: '))
+            x = int(input('enter number: ')) 
         except:
             print('thats not a number')
             continue
         else:
+            print(f'{x} + 3 is: ', x + 3)
+            print('{} - 3 is: '.format(x), x - 3)
             print('thats a number')
             break
+        # this will run whether or not there is an error
+        # so this can be used to make sure you properly close down certain things
+        # such as database or something that needs to be done regardless if the code is successful or not
+        finally:
+            pass
+
 
 in1()
---
+++ pylint.py
@@ -0,0 +1,7 @@
+a = 1
+print(A)
+
+#pylint will show you error you have in code
+# go to cmd direct your cd to where the file you want to test is
+# run this after locating your python file through cmd: py -m pylint file.py
+# check for the error that has the letter E in it for example: E0602
