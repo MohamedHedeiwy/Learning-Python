e4a2f870bafeaf35d16df6db7c94eddf90e9aaa5
Hedeiwy <mhedeiwym@gmail.com>
2020-11-27 23:15:12 +0800

    Error_Handling

--- Error_Handling.py
+++ Error_Handling.py
@@ -1,2 +1,37 @@
-my = [1, 2, 3]
-help(my.insert())
+def add(a,b):
+    try:
+        print(a+b)
+    except:
+        print('you got an error')
+    else:
+        print('you got no error')
+
+def add1(a,b):
+    try:
+        x = a+b
+    except:
+        print('you got an error')
+    else:
+        print('you got no error')
+        return print(x)
+
+print('add')
+add(12,2)
+
+print()
+
+print('add1')
+add1(12,2)
+
+def in1():
+    while True:
+        try:
+            x = int(input('enter number: '))
+        except:
+            print('thats not a number')
+            continue
+        else:
+            print('thats a number')
+            break
+
+in1()
