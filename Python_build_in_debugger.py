dda091aeb773ab83da055d75572dfe60d14f4182
Hedeiwy <mhedeiwym@gmail.com>
2020-11-28 20:05:51 +0800

    Built in debugger library

---
+++ Python_build_in_debugger.py
@@ -0,0 +1,13 @@
+import pdb
+
+x = [12,23,4]
+y = 12
+z = 2
+
+#put it on top of the code that you think has an error, for example on top of a function or calculations like down below
+# it will inspect everything down below not just errors
+# press q to exsit the debugger
+pdb.set_trace()
+a = z + y 
+b = x + y
+c = y + x
